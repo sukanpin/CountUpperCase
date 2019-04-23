1af39961454dfd5ddb85538a8a59681a46a4ce2a
sukanpin <mashu.tsujita@gmail.com>
2019-04-23 20:38:54 +0900

    first commit

---
+++ CountUpperCase/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CountUpperCase
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
